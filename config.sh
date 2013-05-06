54b52598164368877a6bda03a7c1f143edbc9775
Justin Dugger <jldugger@osuosl.org>
2013-05-06 11:57:02 -0700

    add a few perl dependencies for sync script

--- config.sh
+++ config.sh
@@ -6,3 +6,8 @@ apt-get install -y virtualenvwrapper
 apt-get install -y libsasl2-dev
 apt-get install -y libldap2-dev
 apt-get install -y python2.6-dev
+apt-get install -y ldap-utils
+apt-get install -y postgresql
+apt-get install -y libapache2-mod-wsgi
+apt-get install -y libnet-oauth-perl
+apt-get install -y libmail-imapclient-perl 
